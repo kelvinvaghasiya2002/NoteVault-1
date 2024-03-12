ccf2d3b6f847882c12d7aedcf9bab4088de3c378
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-03-12 16:42:57 +0530

    Created sign in and Sign up pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { createBrowserRouter , RouterProvider } from "react-router-dom";
+import SignIn from './components/sign-in/SignIn';
+import SignUp from './components/sign-up/SignUp.jsx';
 
 
 const router = createBrowserRouter([
   {
     path:"/",
     element : <App />
+  },
+  {
+    path:"/sign-in",
+    element : <SignIn />
+  },
+  {
+    path : "/sign-up",
+    element : <SignUp />
   }
 ])
 
